9c5042526e9d34c0ab2658870f0ea804f4b17a42
mariiabogdanova <februarymary@gmail.com>
2018-10-03 12:36:00 +0300

    Exercise 1 finished

--- Exercise 1/node-debugger/app.js
+++ Exercise 1/node-debugger/app.js
@@ -1,5 +1,18 @@
-let name = 'Mariia'; 
+//let name = 'Mariia'; 
 
-debugger;
+//debugger;
 
-console.log(`Hello ${ name }`);
+//console.log(`Hello ${ name }`);
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    //debugger;
+    res.send('Hey, it worked');
+});
+
+//app.listen(3000, () => console.log('Server is up'));
+app.listen(PORT, () => console.log(`Server is up on ${PORT}`));
